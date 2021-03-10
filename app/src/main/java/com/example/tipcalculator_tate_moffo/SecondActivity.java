14134d8eac826812ac887441fc25b62cf61e36fb
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 22:49:18 -0500

    Created second activity and UI elements

---
+++ app/src/main/java/com/example/tipcalculator_tate_moffo/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.tipcalculator_tate_moffo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
