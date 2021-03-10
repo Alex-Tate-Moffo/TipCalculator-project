4554a068bcc76d9776d7859fd43333c8bc36aa2c
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 22:26:45 -0500

    Implemented value-checking

--- app/src/main/java/com/example/tipcalculator_tate_moffo/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_tate_moffo/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +20,18 @@ public class MainActivity extends AppCompatActivity {
     private Button calculateButton;
     private Button resetButton;
 
+    private EditText inputBill;
+    private EditText inputPeople;
+    private EditText inputCustom;
+
+    private RadioButton fifteenSelected;
+    private RadioButton twentySelected;
+    private RadioButton twentyFiveSelected;
+    private RadioButton customSelected;
+
     private float bill;
     private int numPeople;
-    // FIX? default value should instead be "you must select" message?
-    private float tipPercent = 10;
+    private float tipPercent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,29 +45,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-        /*calculateButton.setOnClickListener(new View.OnClickListener(){
+        calculateButton = findViewById(R.id.button_calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                i.putExtra("bill", bill);
-                i.putExtra("numPeople", numPeople);
-                i.putExtra("tipPercent", tipPercent);
+                boolean valuesEntered = false;
 
-                startActivity(i);
+
+                inputBill = findViewById(R.id.input_bill);
+                inputPeople = findViewById(R.id.input_people);
+                inputCustom = findViewById(R.id.input_percent);
+
+                fifteenSelected = findViewById(R.id.radio_15);
+                twentySelected = findViewById(R.id.radio_20);
+                twentyFiveSelected = findViewById(R.id.radio_25);
+                customSelected = findViewById(R.id.radio_custom);
+
+
+                // if the bill field has a value and the value is >= 1 AND
+                // the num people field has a value and the value is >= 1
+                if (   (!inputBill.getText().toString().equals("") &&
+                        Float.parseFloat(inputBill.getText().toString()) >= 1.0)    &&
+                       (!inputPeople.getText().toString().equals("") &&
+                        Integer.parseInt((inputPeople.getText().toString())) >= 1 ) ) {
+                    // if the custom tip percentage radio button is selected
+                    // and the input field is empty or has a value < 1%
+                    if ((customSelected.isChecked() &&
+                        (inputCustom.getText().toString().equals("") ||
+                         Float.parseFloat(inputCustom.getText().toString()) < 1.0 ))) {
+
+                         Toast.makeText(getApplicationContext(),
+                                "Must enter tip >= 1%", Toast.LENGTH_SHORT).show();
+                    }
+                    // if the custom tip percentage radio button is selected
+                    // and the value is >= 1%
+                    else if ((customSelected.isChecked() &&
+                            Float.parseFloat(inputCustom.getText().toString()) >= 1.0 )) {
+
+                        // GO TO SECOND ACTIVITY
+
+                        Toast.makeText(getApplicationContext(),
+                                "CALCULATING (custom)", Toast.LENGTH_SHORT).show();
+
+                    }
+                    // if a radio button other than custom is selected
+                    else if (fifteenSelected.isChecked() ||
+                             twentySelected.isChecked()  ||
+                             twentyFiveSelected.isChecked() ) {
+
+                        // GO TO SECOND ACTIVITY
+
+                        Toast.makeText(getApplicationContext(),
+                                "CALCULATING (radio)", Toast.LENGTH_SHORT).show();
+
+                    }
+                    // if no radio buttons are selected
+                    else {
+                        Toast.makeText(getApplicationContext(),
+                                "Must select tip %", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                // if something does not have a proper value
+                else {
+                    Toast.makeText(getApplicationContext(),
+                            "Must enter values", Toast.LENGTH_SHORT).show();
+                }
+
             }
-        });*/
+        });
     }
 
     public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
+        // determines if the button checked
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
         // Assign the tip percent
         switch(view.getId()) {
-            case R.id.radio_10:
-                if (checked)
-                    tipPercent = 10;
-                    break;
             case R.id.radio_15:
                 if (checked)
                     tipPercent = 15;
@@ -80,6 +143,16 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, String.valueOf(tipPercent));
     }
+
+    // ...
+
+    // function that goes to the second activity
+
+        /*i.putExtra("bill", bill);
+        i.putExtra("numPeople", numPeople);
+        i.putExtra("tipPercent", tipPercent);
+
+        startActivity(i);*/
 
     // ...
 
